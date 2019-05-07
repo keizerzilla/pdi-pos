a6aa51e7332387a2bb040e3b0d69f396b720c134
Artur Rodrigues Rocha Neto <keizerzilla@gmail.com>
2019-05-07 18:18:18 -0300

    apresentacao parkinson

--- parkinsons/analysis.py
+++ parkinsons/analysis.py
@@ -1,5 +1,9 @@
 import pandas as pd
+from sklearn.metrics import f1_score
 from sklearn.metrics import accuracy_score
+
+df = pd.read_csv("parkinsons.csv")
+print("#atributos:", len(df.columns))
 
 scores = pd.read_csv("scores.csv")
 f1s = pd.read_csv("f1s.csv")
@@ -11,5 +15,6 @@ print(f1s.mean())
 print(mccs.mean())
 
 score_vote = accuracy_score(voting["true"], voting["voted"])
-print("voting:", round(score_vote, 2))
+f1_vote = f1_score(voting["true"], voting["voted"])
+print("voting:", round(score_vote, 2), round(f1_vote, 2))
 
